40bd991d6b05cf2833c7cf09eefb80d66af7f5d4
CafeCat <leonore1982@gmail.com>
2014-06-06 03:54:00 +0800

    start working on cloning

--- clone-files.js
+++ clone-files.js
@@ -1,1 +1,18 @@
-console.log('test-cloning files');
+var cloneFiles = function(_projectDirectory) {
+  var fileTypes = ['php', 'js', 'html', 'htm'];
+  var fs = require('fs');
+  this.starts = function() {
+    fs.exists(_projectDirectory, function(exists) {
+      if (exists) {
+        console.log(' read dir called');
+        fs.readdir(_projectDirectory, function(err, files) {
+          console.log(files);
+        });
+      } else {
+        console.log('directory doens\'t exist.');
+      }
+    });
+  };
+};
+var doIt = new cloneFiles('c:\\wamp\\www');
+doIt.starts();
